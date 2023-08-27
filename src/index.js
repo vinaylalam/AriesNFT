10ff58bda5cbbbda767b1cd76623921000a45fa1
vinay <89630849+vinaylalam@users.noreply.github.com>
2023-08-27 21:07:41 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./state";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { theme } from "./theme";
-import { Auth0Provider } from '@auth0/auth0-react';
+'use strict';
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-j7a6mhh1i8q1aazd.us.auth0.com"
-      clientId="OnJq7dCXtsT8QYSSkPGBI5stW3T0wQzW"
-      authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Provider>
-    </Auth0Provider>
-  </React.StrictMode>
-);
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
